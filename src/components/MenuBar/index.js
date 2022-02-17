5a1eaf1793f960f330d4661a5dc00252807ee18d
Rai Soares <raisoarestricolor98@gmail.com>
2022-02-17 10:04:22 -0300

    add: dark/light themes

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import { Home } from '@styled-icons/boxicons-solid/Home'
 import { SearchAlt2 as Search } from '@styled-icons/boxicons-regular/SearchAlt2'
@@ -8,8 +8,16 @@ import { Grid } from '@styled-icons/boxicons-solid/Grid'
 
 import * as S from "./styles"
 
-const MenuBar = () => (
-  <S.MenuBarWrapper>
+const MenuBar = () => {
+  const [theme, setTheme] = useState(null)
+  const isDarkMode = theme === "dark"
+
+  useEffect(() => {
+    setTheme(window.__theme)
+    window.__onThemeChange = () => setTheme(window.__theme)
+  }, [])
+
+  return <S.MenuBarWrapper>
     <S.MenuBarGroup>
       <S.MenuBarLink to="/" title="Voltar para Home">
         <S.MenuBarItem>
@@ -23,7 +31,9 @@ const MenuBar = () => (
       </S.MenuBarLink>
     </S.MenuBarGroup>
     <S.MenuBarGroup>
-      <S.MenuBarItem title="Mudar o tema">
+      <S.MenuBarItem className={theme} title="Mudar o tema" onClick={() => {
+        window.__setPreferredTheme(isDarkMode ? 'light' : 'dark')
+      }}>
         <Light />
       </S.MenuBarItem>
       <S.MenuBarItem title="Mudar visualização">
@@ -34,6 +44,6 @@ const MenuBar = () => (
       </S.MenuBarItem>
     </S.MenuBarGroup>
   </S.MenuBarWrapper>
-)
+}
 
 export default MenuBar
